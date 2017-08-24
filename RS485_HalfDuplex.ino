01b795e027f8130c1961783c8604d189d6d96bc6
Dave <dave@chapmanhall.co.nz>
2017-08-24 21:56:33 +1200

    Initial commit
    Test code for MEGA 2560 with RS485 interface as a ModBus master

---
+++ RS485_HalfDuplex.ino
@@ -0,0 +1,107 @@
+/*
+
+  RS485_HalfDuplex.pde - example using ModbusMaster library to communicate
+  with EPSolar LS2024B controller using a half-duplex RS485 transceiver.
+
+  This example is tested against an EPSolar LS2024B solar charge controller.
+  See here for protocol specs:
+  http://www.solar-elektro.cz/data/dokumenty/1733_modbus_protocol.pdf
+
+  Library:: ModbusMaster
+  Author:: Marius Kintel <marius at kintel dot net>
+
+  Copyright:: 2009-2016 Doc Walker
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+
+#include <ModbusMaster.h>
+
+/*!
+  We're using a MAX485-compatible RS485 Transceiver.
+  Rx/Tx is hooked up to the hardware serial port at 'Serial'.
+  The Data Enable and Receiver Enable pins are hooked up as follows:
+*/
+#define MAX485_DE      3
+#define MAX485_RE_NEG  2
+
+#define SLAVE_ID    (1)
+
+// instantiate ModbusMaster object
+ModbusMaster node;
+
+void preTransmission()
+{
+  digitalWrite(MAX485_RE_NEG, 1);
+  digitalWrite(MAX485_DE, 1);
+}
+
+void postTransmission()
+{
+  digitalWrite(MAX485_RE_NEG, 0);
+  digitalWrite(MAX485_DE, 0);
+}
+
+void setup()
+{
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  pinMode(MAX485_RE_NEG, OUTPUT);
+  pinMode(MAX485_DE, OUTPUT);
+  // Init in receive mode
+  digitalWrite(MAX485_RE_NEG, 0);
+  digitalWrite(MAX485_DE, 0);
+  
+  Serial.begin(9600);
+  
+  Serial.println("Starting...");
+
+  // Modbus communication runs at 2400 baud
+  Serial1.begin(2400);
+
+  // Modbus slave ID 1
+  node.begin(SLAVE_ID, Serial1);
+  // Callbacks allow us to configure the RS485 transceiver correctly
+  node.preTransmission(preTransmission);
+  node.postTransmission(postTransmission);
+}
+
+void loop()
+{
+  uint8_t result;
+  uint16_t data[6];
+  
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  // Read 2 input registers (4 bytes) starting at 0x0006; 30007 Amps)
+  result = node.readInputRegisters(0x0006, 2);
+  
+  if (result == node.ku8MBSuccess)
+  {
+    Serial.print("Amps: ");
+    Serial.println(node.getResponseBuffer(0x04)/100.0f);
+    Serial.print("Pload: ");
+    Serial.println((node.getResponseBuffer(0x0D) +
+                    node.getResponseBuffer(0x0E) << 16)/100.0f);
+  }
+  else {
+    Serial.println("Read failed");
+  }
+  
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+
+  delay(10000);
+}
+
